5c142832fd5027c454b20e50bb8fb283687daa31
Waldyn Benbenek <wben1201@gmail.com>
2019-01-24 13:42:33 -0600

    The Checkout reducer and path should be working.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import logger from 'redux-logger';
-
+import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 //reducers
-
+const checkout = (state = [], action) => {
+if( action.type === 'SEND_CHECKOUT'){
+return[...state,action.payload]
+}
+else if(action.type === 'CLEAR_CART'){
+    return []
+}
+return state
+}
 
 const storeInstance = createStore(
     combineReducers({
-
+checkout()
     }),
     applyMiddleware(logger),
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>,
+    document.getElementById('root'));
