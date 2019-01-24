54c152d45f2446734d511d5fee347297069d31ab
Britt Carter <brittbmh@gmail.com>
2019-01-24 14:43:00 -0600

    add conditional rendering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './Header.css';
-import {connect}  from 'react-redux'; 
+import { connect } from 'react-redux';
 
 class Header extends Component {
-    render () {
+
+    render() {
+        let cartTotal;
+        if (this.props.reduxStore.Reducer === 0) {
+            cartTotal = <br />
+        } else {
+            cartTotal = <p>Cart Total:</p>
+        }
         return (
             <header className="App-header">
                 <h1 className="App-title">Prime Pizza</h1>
-                <p>Cart Total:</p>
+                {cartTotal}
             </header>
         )
     }
